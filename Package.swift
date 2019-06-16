d017bc70812f0bf827bb2beba15353044e31b394
Jane Fraser <janef0421@gmail.com>
2019-06-17 09:56:52 +1200

    no message

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "Cairo",
-            targets: ["Cairo"]),
+            targets: ["Cairo"])
     ],
     targets: [
         .target(
